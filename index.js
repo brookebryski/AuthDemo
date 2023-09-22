5be2bcd240e093dacf92a1dd983db57ea66f96dc
BrookeBryski <brookeabryski@gmail.com>
2023-09-22 15:47:53 -0700

    moved static methods to models

--- index.js
+++ index.js
@@ -20,6 +20,13 @@ app.set('views', 'views');
 app.use(express.urlencoded({ extended: true}));
 app.use(session({ secret: 'notagoodsecret'}))
 
+const requireLogin = (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    }
+    next();
+}
+
 app.get('/', (req, res) => {
     res.send('Welcome Home');
 })
@@ -30,11 +37,7 @@ app.get('/register', (req, res) => {
 
 app.post('/register', async (req, res) => {
     const { password, username } = req.body;
-    const hash = await bcrypt.hash(password, 12);
-    const user = new User ({
-        username,
-        password: hash
-    })
+    const user = new User({ username, password });
     await user.save();
     req.session.user_id = user._id;
     res.redirect('/');
@@ -46,10 +49,9 @@ app.get('/login', (req, res) => {
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        req.session.user_id = user._id;
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
         res.redirect('/secret');
     } else {
         res.redirect('/login');
@@ -61,7 +63,7 @@ app.post('/logout', (req, res) => {
     res.redirect('/login');
 })
 
-app.get('/secret', (req, res) => {
+app.get('/secret', requireLogin, (req, res) => {
     if (!req.session.user_id) {
         return res.redirect('/login');
     }
